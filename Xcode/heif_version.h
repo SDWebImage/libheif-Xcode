5fd3334f9fe6898f1a5b2cfdb5383ef2f8683565
DreamPiggy <lizhuoli1126@126.com>
2019-03-23 10:25:31 +0800

    Upgrade the libheif to upstream 1.4.0

--- Xcode/heif_version.h
+++ Xcode/heif_version.h
@@ -28,9 +28,9 @@
 #define LIBHEIF_HEIF_VERSION_H
 
 /* Numeric representation of the version */
-#define LIBHEIF_NUMERIC_VERSION 0x01030200
+#define LIBHEIF_NUMERIC_VERSION 0x01040000
 
 /* Version string */
-#define LIBHEIF_VERSION "1.3.2"
+#define LIBHEIF_VERSION "1.4.0"
 
 #endif  // LIBHEIF_HEIF_VERSION_H

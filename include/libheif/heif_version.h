7d3920cf12f1273babf653eb0aeef85b361093a5
DreamPiggy <lizhuoli1126@126.com>
2020-09-30 00:46:14 +0800

    Update the podspec and version file to 1.9.0

--- include/libheif/heif_version.h
+++ include/libheif/heif_version.h
@@ -28,9 +28,9 @@
 #define LIBHEIF_HEIF_VERSION_H
 
 /* Numeric representation of the version */
-#define LIBHEIF_NUMERIC_VERSION 0x01080000
+#define LIBHEIF_NUMERIC_VERSION 0x01090000
 
 /* Version string */
-#define LIBHEIF_VERSION "1.8.0"
+#define LIBHEIF_VERSION "1.9.0"
 
 #endif  // LIBHEIF_HEIF_VERSION_H

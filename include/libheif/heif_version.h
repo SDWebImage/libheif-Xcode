94e0b3266f7499f854d7547f68786dfa0785deac
DreamPiggy <lizhuoli1126@126.com>
2022-05-30 17:51:12 +0800

    Upgrade to 1.12.0

--- include/libheif/heif_version.h
+++ include/libheif/heif_version.h
@@ -28,9 +28,9 @@
 #define LIBHEIF_HEIF_VERSION_H
 
 /* Numeric representation of the version */
-#define LIBHEIF_NUMERIC_VERSION 0x01110000
+#define LIBHEIF_NUMERIC_VERSION 0x01120000
 
 /* Version string */
-#define LIBHEIF_VERSION "1.11.0"
+#define LIBHEIF_VERSION "1.12.0"
 
 #endif  // LIBHEIF_HEIF_VERSION_H

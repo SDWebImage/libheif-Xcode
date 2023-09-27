b85f589334dd1f69bee7b962c07471b4d7303a3a
DreamPiggy <lizhuoli1126@126.com>
2023-09-27 21:33:26 +0800

    Upstream to v1.16.1

--- include/libheif/heif_version.h
+++ include/libheif/heif_version.h
@@ -28,9 +28,9 @@
 #define LIBHEIF_HEIF_VERSION_H
 
 /* Numeric representation of the version */
-#define LIBHEIF_NUMERIC_VERSION 0x01160000
+#define LIBHEIF_NUMERIC_VERSION 0x01161000
 
 /* Version string */
-#define LIBHEIF_VERSION "1.16.0"
+#define LIBHEIF_VERSION "1.16.1"
 
 #endif  // LIBHEIF_HEIF_VERSION_H

f11fadac5da056a0bf553cb6cfa8ea26feddf00e
DreamPiggy <lizhuoli1126@126.com>
2024-08-12 17:49:34 +0800

    Updated to v1.17.0

--- Package.swift
+++ Package.swift
@@ -65,7 +65,7 @@ if HAVE_X265 {
 let package = Package(
     name: "libheif",
     platforms: [
-        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)
+        .macOS(.v10_11), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
